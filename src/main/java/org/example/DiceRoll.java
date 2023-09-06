00cb1372aec22f00f44333b41929feda309c611f
shonanohs <shonafitzpatrick15@gmail.com>
2023-09-06 14:32:36 +0100

    Initial commit

---
+++ src/main/java/org/example/DiceRoll.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class DiceRoll {
+    public static void main(String[] args) {
+        int[] diceNums = {1, 2, 3, 4, 5, 6};
+
+        int max = 6;
+        int min = 1;
+        int range = max - min + 1;
+        int roll = (int) (Math.random() * range) + min;
+            System.out.println(roll);
+        }
+    }
