8c882a4c262521e786e39994e1aa0213fcc286df
shonanohs <shonafitzpatrick15@gmail.com>
2023-09-06 15:34:54 +0100

    Fixed bug with dice roll

--- src/main/java/org/example/DiceRoll.java
+++ src/main/java/org/example/DiceRoll.java
@@ -26,29 +26,35 @@ public class DiceRoll {
             }
         }
 
-
     }
     private static void rollDice() {
+        Scanner getInput = new Scanner(System.in);
+
         // Set min and max values for roll
         int max = 6;
         int min = 1;
         int range = max - min + 1;
         // Generate number at random between min and max numbers inclusive
         int roll1 = (int) (Math.random() * range) + min;
-        System.out.println("Let's go! Your first roll is " + roll1); // Print out initial roll
+        System.out.println("Let's go! Your initial roll is " + roll1); // Print out initial roll
 
         // Keep rolling dice while the rolls do not match the initial roll
         int count = 0;
-        int roll2;
-        do {
+        int roll2 = 0;
+        while (roll1 != roll2) {
             roll2 = (int) (Math.random() * range) + min;
+            if (roll1 == roll2) {
+                break;
+            }
             System.out.println("Roll " + (count + 1) + ": " + roll2);
+            System.out.println("Try again!");
+            getInput.nextLine();
             count++;
         }
-        while (roll1 != roll2);
 
         // Print out the number of rolls required to match initial roll
-        System.out.println("\nNumber of rolls: " + count);
+        System.out.println("Final roll: " + roll2);
+        System.out.println("You did it, well done! It took " + count + " rolls.");
     }
     private static void drawDice() {
         System.out.println("       .-------.    ______\n" +
