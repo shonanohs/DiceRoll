4538ad27bd6594318bfff0efbe79b44f3806acf0
shonanohs <shonafitzpatrick15@gmail.com>
2023-09-06 15:16:01 +0100

    Added user input

--- src/main/java/org/example/DiceRoll.java
+++ src/main/java/org/example/DiceRoll.java
@@ -1,18 +1,62 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class DiceRoll {
     public static void main(String[] args) {
-        int[] diceNums = {1, 2, 3, 4, 5, 6};
+        Boolean shouldRepeat = true;
 
+        Scanner getInput = new Scanner(System.in);
+
+        while (shouldRepeat) {
+            drawDice();
+            System.out.println("Would you like to roll the dice? ");
+            String UserResponse = getInput.nextLine();
+
+            if (UserResponse.equalsIgnoreCase("yes") || UserResponse.equalsIgnoreCase("y")) {
+                shouldRepeat = false;
+                rollDice();
+            }
+            else if (UserResponse.equalsIgnoreCase("no") || UserResponse.equalsIgnoreCase("n")) {
+                System.out.println("Ok... see you next time! ");
+                shouldRepeat = false;
+            }
+            else {
+                System.out.println("Invalid input. Please enter yes/no. ");
+            }
+        }
+
+
+    }
+    private static void rollDice() {
+        // Set min and max values for roll
         int max = 6;
         int min = 1;
         int range = max - min + 1;
-        // Roll dice
+        // Generate number at random between min and max numbers inclusive
         int roll1 = (int) (Math.random() * range) + min;
-        System.out.println(roll1);
+        System.out.println("Let's go! Your first roll is " + roll1); // Print out initial roll
 
+        // Keep rolling dice while the rolls do not match the initial roll
+        int count = 0;
+        int roll2;
+        do {
+            roll2 = (int) (Math.random() * range) + min;
+            System.out.println("Roll " + (count + 1) + ": " + roll2);
+            count++;
+        }
+        while (roll1 != roll2);
 
-
-
+        // Print out the number of rolls required to match initial roll
+        System.out.println("\nNumber of rolls: " + count);
+    }
+    private static void drawDice() {
+        System.out.println("       .-------.    ______\n" +
+                "      /   o   /|   /\\     \\\n" +
+                "     /_______/o|  /o \\  o  \\\n" +
+                "     | o     | | /   o\\_____\\\n" +
+                "     |   o   |o/ \\o   /o    /\n" +
+                "     |     o |/   \\ o/  o  /\n" +
+                "     '-------'     \\/____o/");
     }
 }
