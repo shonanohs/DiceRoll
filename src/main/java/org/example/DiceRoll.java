345be72136cd225e3da06ba6e8790f2ea4a3b9ac
shonanohs <shonafitzpatrick15@gmail.com>
2023-09-06 16:54:01 +0100

    Edited game instructions

--- src/main/java/org/example/DiceRoll.java
+++ src/main/java/org/example/DiceRoll.java
@@ -21,6 +21,9 @@ public class DiceRoll {
                 if (userResponse.equalsIgnoreCase("yes") || userResponse.equalsIgnoreCase("y")) {
                     shouldRepeat = true;
                 }
+                else {
+                    System.out.println("Thanks for playing Dice Roller!");
+                }
             }
             else if (userResponse.equalsIgnoreCase("no") || userResponse.equalsIgnoreCase("n")) {
                 System.out.println("Ok... see you next time! ");
@@ -30,8 +33,8 @@ public class DiceRoll {
                 System.out.println("Invalid input. Please enter yes/no. ");
             }
         }
-
     }
+
     private static void rollDice() {
         Scanner getInput = new Scanner(System.in);
 
@@ -41,29 +44,29 @@ public class DiceRoll {
         int range = max - min + 1;
         // Generate number at random between min and max numbers inclusive
         int roll1 = (int) (Math.random() * range) + min;
-        System.out.println("Let's go! Your initial roll is " + roll1); // Print out initial roll
+        System.out.println("Let's go! Press enter to roll the dice.");
+        getInput.nextLine();
+        System.out.println("Your initial roll is " + roll1 + ". Now let's see how many rolls it takes " +
+                "you to match it!"); // Print out initial roll
+        getInput.nextLine();
 
         // Keep rolling dice while the rolls do not match the initial roll
         int count = 0;
         int roll2 = 0;
         while (roll1 != roll2) {
+            count++;
             roll2 = (int) (Math.random() * range) + min;
+            System.out.println("Roll " + (count) + ": " + roll2);
             if (roll1 == roll2) {
-                if (count == 0) {
-                    count++;
-                }
+                // Print out the number of rolls required to match initial roll
+                System.out.println("\nYou did it, well done! It took " + count + " roll(s).\n");
                 break;
             }
-            System.out.println("Roll " + (count + 1) + ": " + roll2);
             System.out.println("Try again!");
             getInput.nextLine();
-            count++;
         }
-
-        // Print out the number of rolls required to match initial roll
-        System.out.println("Final roll: " + roll2);
-        System.out.println("You did it, well done! It took " + count + " roll(s).");
     }
+
     private static void drawDice() {
         System.out.println("       .-------.    ______\n" +
                 "      /   o   /|   /\\     \\\n" +
